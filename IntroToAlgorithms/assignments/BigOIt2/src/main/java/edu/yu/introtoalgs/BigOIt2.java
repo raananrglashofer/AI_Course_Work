e985b4b262a0e4923e8816200b0b1944e1ec623d
raananrglashofer <raananrg@gmail.com>
2023-09-27 17:22:53 -0400

    5:22PM 9/27

---
+++ IntroToAlgorithms/assignments/BigOIt2/src/main/java/edu/yu/introtoalgs/BigOIt2.java
@@ -0,0 +1,8 @@
+package edu.yu.introtoalgs;
+
+public class BigOIt2 extends BigOIt2Base{
+    @Override
+    public double doublingRatio(String bigOMeasurable, long timeOutInMs) {
+        return 0;
+    }
+}
