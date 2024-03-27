adec7dfec5ab03f27f4804ba694916bc2322f686
raananrglashofer <raananrg@gmail.com>
2024-03-26 22:21:23 -0400

    3/26 10:21PM

--- DesignAndAnalysis/assignments/SpheresOfInfluence/src/main/java/edu/yu/da/SpheresOfInfluence.java
+++ DesignAndAnalysis/assignments/SpheresOfInfluence/src/main/java/edu/yu/da/SpheresOfInfluence.java
@@ -1,24 +1,48 @@
 package edu.yu.da;
 
 import java.util.*;
+import java.lang.*;
 
 
 public class SpheresOfInfluence extends SpheresOfInfluenceBase{
     private int maxStrength;
     private int maxRight;
-    private List<String> minimalCoverageInfluencers = new ArrayList<>();
     private Set<Influencer> influencers = new HashSet<>();
     private Set<String> ids = new HashSet<>();
+    private List<Influencer> validInfluencers = new ArrayList<>();
 
     private class Influencer{
         private String id;
         private int xValue;
         private int radius;
+        private int leftMostIntersection; // these calculations are off and need to be using pythagorean theorem
+        private int rightMostIntersection;// these calculations are off and need to be using pythagorean theorem
 
         public Influencer(String id, int xValue, int radius){
             this.id = id;
             this.xValue = xValue;
             this.radius = radius;
+            // can't do the intersection for all influencers because they don't all reach maxStrength
+//            this.leftMostIntersection = xValue - radius; // need to change to pythagorean theorem
+//            this.rightMostIntersection = xValue + radius; // need to change to pythagorean theorem
+        }
+
+        @Override
+        public boolean equals(Object obj){
+            if(this == obj){
+                return true;
+            }
+            if(obj == null || getClass() != obj.getClass()){
+                return false;
+            }
+
+            Influencer otherInfluencer = (Influencer) obj;
+            return (xValue == otherInfluencer.xValue && radius == otherInfluencer.radius);
+        }
+
+        @Override
+        public int hashCode() { // maybe change the hash function and don't use objects
+            return Objects.hash(xValue, radius);
         }
     }
     /**
@@ -60,6 +84,7 @@ public class SpheresOfInfluence extends SpheresOfInfluenceBase{
         }
         Influencer influencer = new Influencer(id, xValue, radius);
         this.ids.add(id);
+        // could already filter here
         this.influencers.add(influencer);
     }
 
@@ -70,11 +95,83 @@ public class SpheresOfInfluence extends SpheresOfInfluenceBase{
      * supplied influencers can provide complete coverage.  The ids MUST BE
      * sorted in order of increasing lexicographical order.
      */
+
+    // Filter influencers that go beyond the maxStrength (radius > maxStrength / 2)
+    // sort by furthest to the left  - sort on xValue (nlogn)
+    // greedily take xValue that goes the farthest (current xValue to next point of intersection)
+    // more or less operates as a direct line down from first intersection point to next intersection point and take that rectangle
     @Override
     public List<String> getMinimalCoverageInfluencers() {
-        if(this.minimalCoverageInfluencers == null){
-            return Collections.emptyList();
+        for(Influencer influencer : this.influencers){
+            if(influencer.radius > (this.maxStrength / 2)){
+                this.validInfluencers.add(influencer);
+                setIntersectionPoints(influencer);
+            }
         }
-        return this.minimalCoverageInfluencers;
+        sortValidInfluencers();
+
+        int currentX = 0;
+        List<String> selectedInfluencersIds = new ArrayList<>();
+
+        while (currentX < maxRight) {
+            Influencer bestInfluencer = null;
+            int furthestX = currentX;
+
+            for (Influencer influencer : this.validInfluencers) {
+                // Influencer is eligible if it starts before or at the currentX and extends further than any we've seen so far.
+                if (influencer.leftMostIntersection <= currentX && influencer.rightMostIntersection > furthestX) {
+                    bestInfluencer = influencer;
+                    furthestX = influencer.rightMostIntersection;
+                }
+            }
+
+            if (bestInfluencer == null) {
+                // This means we couldn't find any influencer to extend the coverage from currentX.
+                // Thus, full coverage is impossible with the given set of influencers.
+                return Collections.emptyList();
+            }
+
+            // Add the best influencer for this segment and update currentX to the new furthest right point.
+            selectedInfluencersIds.add(bestInfluencer.id);
+            currentX = furthestX; // Move to the end of the best influencer's coverage
+
+            // Optimization: remove the selected influencer from consideration in future iterations
+            this.validInfluencers.remove(bestInfluencer);
+        }
+
+        // Sort the selected influencers' IDs in lexicographical order before returning.
+        Collections.sort(selectedInfluencersIds);
+        return selectedInfluencersIds;
+    }
+    // I think this math gets the intersection points correctly
+    private void setIntersectionPoints(Influencer influencer){
+        int a = maxStrength / 2;
+        int c = influencer.radius;
+        int b = (int) Math.sqrt((c * c) - (a * a));
+        influencer.leftMostIntersection = influencer.xValue - b;
+        influencer.rightMostIntersection = influencer.xValue + b;
+    }
+
+    private void sortValidInfluencers(){
+        Comparator<Influencer> comparator = new Comparator<>() {
+            // sorts by leftMostPoint and then tiebreaker is rightMostPoint
+            // double check that this sorts in increasing order
+            @Override
+            public int compare(Influencer i1, Influencer i2) {
+                if(i1.leftMostIntersection == i2.leftMostIntersection){
+                    if(i1.rightMostIntersection > i2.rightMostIntersection){
+                        return 1;
+                    } else{
+                        return -1;
+                    }
+                }
+                if(i1.leftMostIntersection > i2.leftMostIntersection){
+                    return 1;
+                } else{
+                    return -1;
+                }
+            }
+        };
+        Collections.sort(this.validInfluencers, comparator);
     }
 }
--- DesignAndAnalysis/assignments/SpheresOfInfluence/src/test/java/SpheresOfInfluenceTest.java
+++ DesignAndAnalysis/assignments/SpheresOfInfluence/src/test/java/SpheresOfInfluenceTest.java
@@ -60,8 +60,18 @@ public class SpheresOfInfluenceTest {
     }
 
     @Test
-    public void simpleLeffTestFail(){
+    public void simpleLeffTestFailBecauseMaxRight(){
         SpheresOfInfluence soi = new SpheresOfInfluence(2, 12);
+        soi.addInfluencer("A", 2, 3);
+        soi.addInfluencer("B", 6, 5);
+        List<String> solution = soi.getMinimalCoverageInfluencers();
+        List<String> expected = Collections.emptyList();
+        // List should be empty
+        assertEquals(solution, expected);
+    }
+    @Test
+    public void simpleLeffTestFailBecauseMaxStrength(){
+        SpheresOfInfluence soi = new SpheresOfInfluence(8, 10);
         soi.addInfluencer("A", 2, 3);
         soi.addInfluencer("B", 6, 5);
         List<String> solution = soi.getMinimalCoverageInfluencers();
@@ -79,8 +89,60 @@ public class SpheresOfInfluenceTest {
         }
         List<String> solution = soi.getMinimalCoverageInfluencers();
         long endTime = System.nanoTime();
-        long time = endTime - startTime;
-        assertTrue(time / 1000000000.0 <= 0.45);
+        long time = (long) ((endTime - startTime) / 1000000000.0);
+        System.out.println("Algorithm time: " + time + " seconds");
+        assertTrue(time <= 0.45);
+    }
+
+    @Test
+    // question from Piazza - 99% covered is still not all covered
+    public void almostAllCovered(){
+        SpheresOfInfluence soi = new SpheresOfInfluence(2, 5);
+        soi.addInfluencer("A", 2, 3);
+        List<String> solution = soi.getMinimalCoverageInfluencers();
+        List<String> expected = Collections.emptyList();
+        // List should be empty
+        assertEquals(solution, expected);
+    }
+
+    @Test
+    public void manyInfluencers(){
+        SpheresOfInfluence soi = new SpheresOfInfluence(8, 10);
+        soi.addInfluencer("A", 2, 3);
+        soi.addInfluencer("B", 7, 5);
+        soi.addInfluencer("C", 3, 5);
+        soi.addInfluencer("D", 1, 3);
+        soi.addInfluencer("E", 2, 4);
+        soi.addInfluencer("F", 2, 1);
+        soi.addInfluencer("G", 2, 3);
+        soi.addInfluencer("H", 3, 3);
+        List<String> solution = soi.getMinimalCoverageInfluencers();
+        List<String> expected = List.of("B", "C");
+        assertEquals(solution, expected);
+    }
+
+    @Test
+    public void initialSortTurnsOutBad(){
+        SpheresOfInfluence soi = new SpheresOfInfluence(8, 10);
+        soi.addInfluencer("A", 2, 5);
+        soi.addInfluencer("B", 3, 5);
+        soi.addInfluencer("C", 7, 5);
+        List<String> solution = soi.getMinimalCoverageInfluencers();
+        List<String> expected1 = List.of("A", "C"); // but could also be B + C
+        List<String> expected2 = List.of("B", "C");
+        assertTrue(solution.equals(expected1) || solution.equals(expected2) );
+    }
+
+    @Test
+    public void needAllThree(){
+        SpheresOfInfluence soi = new SpheresOfInfluence(8, 10);
+        soi.addInfluencer("A", 0, 5);
+        soi.addInfluencer("B", 10, 5);
+        soi.addInfluencer("C", 4, 5);
+        List<String> solution = soi.getMinimalCoverageInfluencers();
+        List<String> expected = List.of("A", "B", "C");
+        System.out.println(solution);
+        assertEquals(solution, expected);
     }
 
     // create test where largest influencer is not in minimal set
@@ -90,4 +152,5 @@ public class SpheresOfInfluenceTest {
     public void tooGreedyTest(){
 
     }
+
 }
