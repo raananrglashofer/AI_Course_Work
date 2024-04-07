626dc3e29ecb1719485a5fae5adaeb06bf00cf15
raananrglashofer <raananrg@gmail.com>
2024-04-07 02:00:25 -0400

    4/7 2:00AM

--- DesignAndAnalysis/assignments/BeatThePricingSchemes/src/main/java/edu/yu/da/BeatThePricingSchemes.java
+++ DesignAndAnalysis/assignments/BeatThePricingSchemes/src/main/java/edu/yu/da/BeatThePricingSchemes.java
@@ -7,10 +7,15 @@ public class BeatThePricingSchemes extends BeatThePricingSchemesBase{
     private double unitPrice;
     private Set<PricingScheme> schemes = new HashSet<>(); // Decide List or Set
     private int count = 1;
+    private double[] bestPrices = new double[MAX_MATZOS+1]; // store optimal prices for each number of Matzos
+    private List<Integer> optimalDecisions = new ArrayList<>();
+    private boolean priceSchemesAdded = false;
+    private Map<Integer, List<Integer>> optimalPriceMap = new HashMap<>(); // store optimal schemes
+    private boolean cheapestPriceCalled = false;
 
     private class PricingScheme{
         private double price;
-        private double quantity;
+        private int quantity;
         private int number; // to keep track which number scheme this is
         public PricingScheme(double price, int quantity, int number){
             this.price = price;
@@ -49,7 +54,6 @@ public class BeatThePricingSchemes extends BeatThePricingSchemesBase{
         this.unitPrice = unitPrice;
         PricingScheme scheme = new PricingScheme(unitPrice, 1, 0);
         this.schemes.add(scheme);
-
     }
     /** Adds a pricing scheme to be considered when making the "select optimal
      * pricing schemes" decision.
@@ -63,11 +67,15 @@ public class BeatThePricingSchemes extends BeatThePricingSchemesBase{
      */
     @Override
     public void addPricingScheme(double price, int quantity) {
-        if(price <= 0 || quantity < 1 || quantity > MAX_MATZOS || this.schemes.size() == 20){
+        if(price <= 0 || quantity < 1 || quantity > MAX_MATZOS || this.schemes.size() == 21){
             throw new IllegalArgumentException();
         }
         PricingScheme scheme = new PricingScheme(price, quantity, this.count++); // I believe this makes it go up and looks cooler
-        this.schemes.add(scheme);
+        // if cost per matzah is greater than unitPrice CPM then never will get used
+        if((scheme.price / scheme.quantity) <= this.unitPrice){
+            this.schemes.add(scheme);
+            this.priceSchemesAdded = true;;
+        }
     }
     /** Returns the cheapest price needed to buy at least threshold items.  Thus
      * the quantity bought may exceed the threshold, as long as that is the
@@ -81,13 +89,66 @@ public class BeatThePricingSchemes extends BeatThePricingSchemesBase{
      * @throw IllegalArgumentException if the parameter pre-conditions are violated.
     //* @see MAX_MATZOS
      */
+    // if priceSchemes were added since last call need to use a new map
     @Override
     public double cheapestPrice(int threshold) {
         if(threshold > MAX_MATZOS || threshold < 1){
             throw new IllegalArgumentException();
         }
-        return 0;
+        this.cheapestPriceCalled = true;
+//        // nothing changed / no price schemes added so don't need to do extra work
+//        // doesn't work if that threshold hasn't been hit yet though
+//        if(optimalPriceMap.containsKey(threshold) && !this.priceSchemesAdded){
+//            return this.bestPrices[threshold];
+//        }
+
+        computeCheapestPrice(threshold);
+        priceSchemesAdded = false; // Reset flag after computation
+
+        // very hacky and should be met with caution
+        Double cheapest = (double) Integer.MAX_VALUE;
+        int index = threshold;
+        for(int i = threshold; i < MAX_MATZOS; i++){
+            if(this.bestPrices[i] < cheapest){
+                cheapest = this.bestPrices[i];
+                index = i;
+            }
+        }
+        this.optimalDecisions = this.optimalPriceMap.get(index);
+        return cheapest;
     }
+
+    private void computeCheapestPrice(int threshold) {
+        // fill price array with unit price
+        this.bestPrices[0] = 0;
+        for(int i = 1; i < MAX_MATZOS + 1; i++){
+            this.bestPrices[i] = i * unitPrice;
+            List<Integer> list = new ArrayList<>();
+            for(int j = 0; j < i; j++){
+                list.add(0);
+            }
+            for(PricingScheme scheme : this.schemes){
+                if(scheme.quantity <= i){
+                    if(this.bestPrices[i] > this.bestPrices[i - scheme.quantity] + scheme.price){
+                        this.bestPrices[i] = this.bestPrices[i - scheme.quantity] + scheme.price;
+
+                        List<Integer> previousBestSchemeList = new ArrayList<>();
+                        if(optimalPriceMap.get(i - scheme.quantity) != null){
+                            previousBestSchemeList = new ArrayList<>(optimalPriceMap.get(i - scheme.quantity));
+                            //list = optimalPriceMap.get(i - scheme.quantity);
+                        }
+                        previousBestSchemeList.add(scheme.number);
+                        list = previousBestSchemeList;
+                    }
+                }
+            }
+           // System.out.println("This is the list for: " + i + " " + list);
+            this.optimalPriceMap.put(i, list);
+        }
+        //System.out.println(optimalPriceMap.get(4));
+    }
+
+
     /** Returns a list of optimal price scheme decisions corresponding to the
      * cheapest price.  If a unit price decision is made, it's represented by the
      * UNIT_PRICE_DECISION constant.  Otherwise, a price scheme is represented by
@@ -98,6 +159,9 @@ public class BeatThePricingSchemes extends BeatThePricingSchemesBase{
      */
     @Override
     public List<Integer> optimalDecisions() {
-        return null;
+        if(!this.cheapestPriceCalled){
+            throw new IllegalStateException();
+        }
+        return this.optimalDecisions;
     }
 }
--- DesignAndAnalysis/assignments/BeatThePricingSchemes/src/test/java/BeatThePricingSchemesTest.java
+++ DesignAndAnalysis/assignments/BeatThePricingSchemes/src/test/java/BeatThePricingSchemesTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import org.junit.Assert;
 import edu.yu.da.BeatThePricingSchemesBase;
 import edu.yu.da.BeatThePricingSchemes;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 import java.util.*;
 import java.util.Collections;
@@ -40,7 +41,7 @@ public class BeatThePricingSchemesTest {
         // Adding 21st pricing scheme
         assertThrows(IllegalArgumentException.class, () -> {
             BeatThePricingSchemesBase test = new BeatThePricingSchemes(4);
-            for(int i = 1; i < 21; i++){
+            for(int i = 1; i < 22; i++){
                 test.addPricingScheme(i, 10);
             }
         });
@@ -56,6 +57,12 @@ public class BeatThePricingSchemesTest {
             BeatThePricingSchemesBase test = new BeatThePricingSchemes(4);
             test.cheapestPrice(0);
         });
+
+        // optimalDecisions() called before cheapestPrice()
+        assertThrows(IllegalStateException.class, () -> {
+            BeatThePricingSchemesBase test = new BeatThePricingSchemes(4);
+            test.optimalDecisions();
+        });
     }
 
     @Test
@@ -64,11 +71,72 @@ public class BeatThePricingSchemesTest {
         btps.addPricingScheme(8.75, 5);
         double cheapest1 = btps.cheapestPrice(4);
         List<Integer> optimalDecisions1 = btps.optimalDecisions();
-        assertNotNull(optimalDecisions1);
-        assertEquals(cheapest1, 8.0);
+        List<Integer> expected1 = List.of(0, 0, 0, 0);
+        assertTrue(cheapest1 == 8.0);
+        assertEquals(optimalDecisions1, expected1);
         double cheapest2 = btps.cheapestPrice(6);
         List<Integer> optimalDecisions2 = btps.optimalDecisions();
-        assertNotNull(optimalDecisions2);
-        assertEquals(cheapest2, 10.75);
+        List<Integer> expected2 = List.of(0, 1);
+        assertTrue(cheapest2 == 10.75);
+        assertEquals(optimalDecisions2, expected2);
+    }
+
+    @Test
+    public void moreMatzosIsCheaper(){
+        BeatThePricingSchemesBase btps = new BeatThePricingSchemes(5.0);
+        btps.addPricingScheme(10.0, 20);
+        btps.addPricingScheme(8.75, 3);
+        btps.addPricingScheme(14.0, 4);
+        btps.addPricingScheme(6.0, 1);
+        double cheapest1 = btps.cheapestPrice(4);
+        List<Integer> optimalDecisions1 = btps.optimalDecisions();
+        List<Integer> expected1 = List.of(1); //  should be the 10 for 20
+        System.out.println(cheapest1);
+        assertTrue(cheapest1 == 10.0);
+        assertEquals(optimalDecisions1, expected1);
+    }
+
+    @Test
+    public void unitPriceIsTheBest(){
+        BeatThePricingSchemesBase btps = new BeatThePricingSchemes(5.0);
+        btps.addPricingScheme(1000.0, 20);
+        btps.addPricingScheme(8.75, 1);
+        btps.addPricingScheme(14.0, 2);
+        btps.addPricingScheme(60.0, 2);
+        btps.addPricingScheme(11, 2);
+        btps.addPricingScheme(95.99, 4);
+        double cheapest1 = btps.cheapestPrice(90);
+        List<Integer> optimalDecisions1 = btps.optimalDecisions();
+        List<Integer> expected1 = new ArrayList<>();
+        for(int i = 0; i < 90; i++){
+            expected1.add(0);
+        }
+        assertEquals(optimalDecisions1, expected1);
+        assertTrue(cheapest1 == 450.0);
+    }
+
+    @Test
+    public void needThreeSchemes(){
+        BeatThePricingSchemesBase btps = new BeatThePricingSchemes(11.0);
+        btps.addPricingScheme(70.0, 7);
+        btps.addPricingScheme(30.0, 3);
+        double cheapest = btps.cheapestPrice(11);
+        assertTrue(cheapest == 111.0);
+        List<Integer> optimalDecisions1 = btps.optimalDecisions();
+        List<Integer> expected1 = List.of(0, 1, 2);
+        assertEquals(optimalDecisions1, expected1);
+    }
+
+    @Test
+    public void wayBetterScheme(){
+        BeatThePricingSchemesBase btps = new BeatThePricingSchemes(100.0);
+        btps.addPricingScheme(1, 10);
+        List<Integer> list = List.of(1);
+        for(int i = 1; i < 11; i++){
+            double cheapest = btps.cheapestPrice(i);
+            assertTrue(cheapest == 1.0);
+            List<Integer> optimalDecisions = btps.optimalDecisions();
+            assertEquals(list, optimalDecisions);
+        }
     }
 }
